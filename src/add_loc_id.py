52d258a0ba2dc09b8b9759d91d9089bc51379818
Szymon Wieczorek <5509131+fivtyen@users.noreply.github.com>
2019-05-27 20:18:45 +0200

    Update add_loc_id.py

--- src/add_loc_id.py
+++ src/add_loc_id.py
@@ -6,13 +6,13 @@ with open('data/locations.json', 'r') as f1:
 locations = [ast.literal_eval(location.strip()) for location in locations]
 ids = [l['_id'] for l in locations]
 
-with open('locations.csv', 'r') as f2:
+with open('data/locations.csv', 'r') as f2:
     old_loc = [line.strip() for line in f2]
 
 new_loc = []
 for i in range(40):
     new_loc.append(old_loc[i]+ ',' + ids[i])
 
-with open('new_locations.csv', 'w') as f3:
+with open('data/new_locations.csv', 'w') as f3:
     for loc in new_loc:
         f3.write(loc + '\n')
